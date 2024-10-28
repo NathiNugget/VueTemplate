dcef7116229231fee649c71b81e0e984344742ec
= <nathaniel.risum2@gmail.com>
2024-10-28 13:50:33 +0100

    Added files to template repo

---
+++ index.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data() {
+      return {
+        count: 0
+      }
+    },
+    methods: {
+
+    },
+
+    computed: {
+        
+    },
+  })
+  
+  app.mount('#app')
